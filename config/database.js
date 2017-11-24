84612e5eb8a6e304c016251fc922e74bc7b3a2d9
gleider <gmdcampos@inf.ufpel.edu.br>
2017-11-23 22:53:06 -0200

    apagar mensagem de remoção

--- config/database.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://localhost/db_finance')
-
-mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
